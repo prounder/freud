767bd7f83a8e7a29abce13595a3e6a5eab018e02
A. Shi <as4ac@virginia.edu>
2019-03-03 05:19:43 -0500

    Added Freud

---
+++ Assets/Scripts/FollowPlayer.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPlayer : MonoBehaviour
+{
+
+	public float speed;
+
+	private GameObject ovrCameraRig;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        ovrCameraRig = GameObject.Find("CenterEyeAnchor");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    	// print("Following!");
+        Vector3 targetDir = ovrCameraRig.transform.position - transform.position;
+        // targetDir.Normalize();
+
+        // // Move our position a step closer to the target.
+        // transform.rotation = Quaternion.Euler(targetDir.x, targetDir.y, targetDir.z);
+        Quaternion newRotation = Quaternion.LookRotation(targetDir) * Quaternion.Euler(0, 90, 0);
+		transform.rotation = Quaternion.Slerp(transform.rotation, newRotation, Time.deltaTime * 1.0f);
+    }
+}
